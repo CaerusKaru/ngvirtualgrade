e14ecd363f56e9efef041c69570fdfb433f23779
Adam Plumer <caerus.karu@gmail.com>
2017-09-11 14:18:00 -0400

    Removed old travis build script fragments

--- scripts/ci/build-dev.sh
+++ scripts/ci/build-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd $TRAVIS_BUILD_DIR/public_html || exit 25
-find . -type d -exec chmod g+rwx {} \; || exit 26
+#cd $TRAVIS_BUILD_DIR/public_html || exit 25
+#find . -type d -exec chmod g+rwx {} \; || exit 26
 #chmod -R a+r . || exit 27
-cd .. || exit 28
+#cd .. || exit 28
 rsync -rp --delete --quiet dist $SSH_USER@$HOST:$DEV_PATH/server/. || exit 29
 rsync -rp --delete --quiet dist-server $SSH_USER@$HOST:$DEV_PATH/server/. || exit 29
 rsync -rp --delete --quiet package.json $SSH_USER@$HOST:$DEV_PATH/server/. || exit 29
